37b05531a8537e1e50e02c004d658d7c470237c5
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-03-02 13:49:33 +0200

    switch axios to nodefetch relates #21

--- server/models/getPosts.js
+++ server/models/getPosts.js
@@ -1,21 +1,17 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 const fs = require('fs');
 const { join } = require('path');
 
-const getPostsApi = (url, res) => {
-  axios
-    .get(url)
-    .then((response) => {
-      const filePath = join(__dirname, 'posts.json');
-      fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) throw err;
-        let postList = JSON.parse(data);
-        postList = [...postList, ...response.data];
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(JSON.stringify(postList));
-      });
-    })
-    .catch();
-};
+const getPostsApi = (url, res) => fetch(url).then((data) => data.json())
+  .then((json) => {
+    const filePath = join(__dirname, 'posts.json');
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) throw err;
+      let postList = JSON.parse(data);
+      postList = [...postList, ...json];
+      res.json(postList);
+    });
+  })
+  .catch();
 
 module.exports = getPostsApi;
